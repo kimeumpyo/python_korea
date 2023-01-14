0cb99876694ff7e89e42fc525e3c7a5187591ca0
kimeumpyo <eumpyo0409@gmail.com>
2023-01-14 13:00:45 +0900

    파이썬 3일 - 제어문

--- day03_2.py
+++ day03_2.py
@@ -5,20 +5,19 @@
 #     print('hello')
 
 # # 임시변수 :  while에서 i를 값으로 사용 ==> 임시변수
-# i = 0
-# while i < 3:
-#     print(i,'번째 Hello')
-#     i += 1
+i = 0
+while i < 3:
+    print(i,'번째 Hello')
+    i += 1
 
-# for j in range(3):
-#     print(j, '번째 Hello')
+for j in range(3):
+    print(j, '번째 Hello')
 
-# # i = 1
-# for i in range(1, 4):           # 1 ~ 3 Hello
-#     print(i, '번째 Hello')
+# i = 1
+for i in range(1, 4):           # 1 ~ 3 Hello
+    print(i, '번째 Hello')
 
-# range(3) == range(0, 3)       # 0 ~ 2
-# range(1, 4) == 1 ~ 4전까지 (1 ~ 3)
+range(3) == range(0, 3)       # 0 ~ 2
 
 # 1월
 # 2월
@@ -31,4 +30,61 @@
 for i in range(1, 13):
     print(i,'월')
 
+# for i in range(7, 101, 7):
+#     print(i)
 
+# for j in range(0, 11, 2):
+#     print(j)
+
+
+# 문제1
+'''
+5
+4
+3
+2
+1
+'''
+for i in range(5, 0, -1):
+    print(i)
+
+# 다른방법1
+num = 5
+for i in range(5):
+    print(num)
+    num -= 1
+
+# 다른방법2
+for i in reversed(range(1, 6)):
+    print(i)
+
+# 문제2
+
+'''
+양의 정수 1개를 입력받아서
+1 부터 입력한 숫자까지 합계를 알려주는 프로그램
+
+ex) 10 
+1 ~ 10을 모두 더해서 55
+ex) 9
+1 ~ 9를 모두 더해서 45
+
+'''
+count = int(input('양의 정수를 입력하세요:'))
+sum = 0   
+
+for i in range(1, count+1):
+    sum += i
+print (sum)
+
+# 문제3
+
+num1 = 1
+num2 = 2
+backup = 0
+backup = num1
+num1 = num2
+num2 = backup
+print(num1, num2)
+    
+    
---
+++ day03_5.py
@@ -0,0 +1,42 @@
+# 문자열 = 'hello world, my name is python'
+# 정수 = 314
+# 실수 = 3.14
+
+# for i in  문자열:
+#     print(i, end=' ')
+
+# print()
+
+# i = 0
+# while i < len(문자열):
+#     print(문자열[i], end=' ')
+#     i += 1
+
+# str, int, float, list, tuple, dict, set
+# 리스트
+# 지하철 3칸, [10, 15, 12]
+subway1 = 10
+subway1 = 10
+subway1 = 10
+print()
+리스트 = [10, 15, 12, 11, 22, 33, 44, 55, 66]
+for i in  리스트:
+    print(i, '명')
+
+
+# 문제1 : 문자열에서 알파벳 o 의 갯수를 알려주세요 
+문자열 = 'hello world, my name is python'
+
+for i in  문자열:
+    if i == 'o':
+        print(i, end='+')
+        o = 1
+        
+        
+
+
+# 문제2 : 1월 ~ 12월을 출력하되 입력받은 월은 skip
+
+# 문제3 : 1월 ~ 12월을 출력하되 입력받은 월로부터는 출력안함
+
+# 문제4 : 구구단을 만들어주세요
