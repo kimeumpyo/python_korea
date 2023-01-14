addfd99d8a91e7b9ab34f67176fd6e78dfa12763
kimeumpyo <eumpyo0409@gmail.com>
2023-01-14 10:51:15 +0900

    파이썬 3일

---
+++ day03_2.py
@@ -0,0 +1,34 @@
+# 반복문 (while, for)
+
+# for를 사용해서 hello 3번하기
+# for 임시변수 in range(3):
+#     print('hello')
+
+# # 임시변수 :  while에서 i를 값으로 사용 ==> 임시변수
+# i = 0
+# while i < 3:
+#     print(i,'번째 Hello')
+#     i += 1
+
+# for j in range(3):
+#     print(j, '번째 Hello')
+
+# # i = 1
+# for i in range(1, 4):           # 1 ~ 3 Hello
+#     print(i, '번째 Hello')
+
+# range(3) == range(0, 3)       # 0 ~ 2
+# range(1, 4) == 1 ~ 4전까지 (1 ~ 3)
+
+# 1월
+# 2월
+# 3월
+# 4월
+# 5월
+# ~~~~
+# 12월
+
+for i in range(1, 13):
+    print(i,'월')
+
+
