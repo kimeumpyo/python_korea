e9846795b3208577843835bdf1156885ba7313fc
kimeumpyo <eumpyo0409@gmail.com>
2023-01-15 10:43:24 +0900

    파이썬 3일 - 제어문

--- day03_5.py
+++ day03_5.py
@@ -25,22 +25,15 @@ for i in  리스트:
 
 
 # 문제1 : 문자열에서 알파벳 o 의 갯수를 알려주세요 
-문자열 = 'hello world, my name is python'
+문자열 = 'hello world, my name is python'             #print(문자열.count('o'),'개')
 
-for i in  문자열:
+o_count = 0
+for i in 문자열:
     if i == 'o':
-        print(i, end='+')
-        o = 1
-        
-        
-
+        o_count += 1
+print(o_count)        
 
 # 문제2 : 1월 ~ 12월을 출력하되 입력받은 월은 skip
-
-# a = int(input('시작월을 입력해주세요>>'))
-# b = int(input('종료될월을 입력해주세요>> '))
-# for i in range(a, b+1):
-#     print(i,'월')
 
 a = int(input('시작월을 입력해주세요>>'))
 b = int(input('종료될월을 입력해주세요>> '))
@@ -52,4 +45,15 @@ for i in range(a, b+1):
    
 # 문제3 : 1월 ~ 12월을 출력하되 입력받은 월로부터는 출력안함
 
+break_month = int(input('몇 월까지 스킵할까요?>>'))
+for i in range(1, 13):
+    if i == break_month:
+        break
+    print(i,'월')
+
+
 # 문제4 : 구구단을 만들어주세요
+for i in range(2,10):
+    for j in range(1,10):
+        print(i, "x", j, "=", i*j, end= '\t')
+    print()                                            # 줄바꿈 
